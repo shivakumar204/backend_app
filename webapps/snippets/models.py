d42249e7b68656c9a27985248351fda3f08c96d0
shivakumar204 <venkypolisettyc@gmail.com>
2024-09-19 11:39:50 +0530

    Update models.py

--- webapps/snippets/models.py
+++ webapps/snippets/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Snippet(models.Model):
-    text = models.TextField()  # the text snippet
-    created_at = models.DateTimeField(auto_now_add=True)  # timestamp of creation
+    text = models.TextField() 
+    created_at = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
-        return self.text[:50]  # return first 50 characters
+        return self.text[:50]  
 
